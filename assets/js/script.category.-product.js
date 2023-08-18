5430926f2be19646624e558132da71d440962f84
julia martins <juliamartinss.564@gmail.com>
2023-08-18 10:18:07 -0300

    feat: create classes

---
+++ assets/js/script.category.-product.js
@@ -0,0 +1,39 @@
+//Cada categoria possui vários produtos
+//Cada produto é pertencente á uma categoria
+
+class Category {
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.products = [];
+    }
+}
+
+class Product{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 1;
+    }
+    
+    addCategory(name){
+        const id = this.nextCategoryId;
+        this.nextCategoryId++;
+
+        const category = new Category (id, name);
+        this.categories.push(category);
+    }
+}
+
+const categoriesList = new CategoryService();
+
+function createCategory(){
+
+}
